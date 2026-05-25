2407718ce03a51677746dd4ecbe3133c6cfb177a
KUS0122 <c5035@oic.jp>
2026-05-25 11:17:57 +0900

    stage7

--- GameCreate18/Assets/Project/Script/ButtonGimmick/stage7/AutoDoorStage7.cs
+++ GameCreate18/Assets/Project/Script/ButtonGimmick/stage7/AutoDoorStage7.cs
@@ -9,9 +9,24 @@ public class AutoDoorStage7 : MonoBehaviour
     private static readonly int OpenHash =
         Animator.StringToHash("Open");
 
+    [SerializeField]
+    private BoxCollider doorBlockCollider;
+
+    [SerializeField]
+    private Vector3 closedColliderCenter;
+
+    [SerializeField]
+    private Vector3 openedColliderCenter;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
+
+        if (doorBlockCollider != null)
+        {
+            closedColliderCenter =
+                doorBlockCollider.center;
+        }
     }
 
     public void SetCanOpen(bool value)
@@ -20,11 +35,49 @@ public class AutoDoorStage7 : MonoBehaviour
 
         if (canOpen)
         {
-            animator.SetBool(OpenHash, true);
+            OpenDoor();
         }
         else
         {
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        if (animator != null)
+        {
+            animator.SetBool(OpenHash, true);
+        }
+
+        MoveColliderOpen();
+    }
+
+    private void CloseDoor()
+    {
+        if (animator != null)
+        {
             animator.SetBool(OpenHash, false);
+        }
+
+        MoveColliderClosed();
+    }
+
+    private void MoveColliderOpen()
+    {
+        if (doorBlockCollider != null)
+        {
+            doorBlockCollider.center =
+                openedColliderCenter;
+        }
+    }
+
+    private void MoveColliderClosed()
+    {
+        if (doorBlockCollider != null)
+        {
+            doorBlockCollider.center =
+                closedColliderCenter;
         }
     }
 }
